faead611316a2d909971a816c9ea4d93e1a6db42
Syed40A <72885427+Syed40H@users.noreply.github.com>
2024-08-07 05:10:51 -0400

    Weather-App-JSREACT 1

---
+++ weather-app-react/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function App() {
+
+    //const url ='https://api.openweathermap.org/data/2.5/weather?q=London&appid=1cb655f2171063cb479d2e53c80869ad'
+
+
+return (
+      <div className="app">
+        <div className="container">
+         <div className="top">
+            <div className="location">
+              <p>London</p>
+            </div>
+            <div className="temp">
+              <h1>60°F</h1>
+            </div>
+            <div className="description">
+              <p>Clouds</p>
+            </div>
+          </div>
+          <div className="bottom">
+            <div className="feels">
+              <p className='bold'>65°F</p>
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              <p className='bold'>20%</p>
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              <p className='bold'>12 MPH</p>
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default App;
